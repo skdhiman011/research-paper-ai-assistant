bec8ac447015ca4e43c4abf7aefba3df2fb4ade3
skdhiman011 <252-25-011@diu.edu.bd>
2026-04-15 12:27:10 +0600

    Create rag_pipeline.py

---
+++ project1_summarizer/src/rag_pipeline.py
@@ -0,0 +1,12 @@
+"""
+rag_pipeline.py
+---------------
+Project 1 not needed any Rag।
+Project 2 nedded RAG
+This file use as placeholder 
+"""
+
+
+def placeholder():
+    """Project 2  module completed।"""
+    pass

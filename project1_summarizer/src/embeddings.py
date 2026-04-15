231fce1902bb40e9c7fd2f6dea80986fe5857c0c
skdhiman011 <252-25-011@diu.edu.bd>
2026-04-15 12:24:58 +0600

    Create embeddings.py as a placeholder file
    Add placeholder function for future embedding implementation.

---
+++ project1_summarizer/src/embeddings.py
@@ -0,0 +1,12 @@
+"""
+embeddings.py
+-------------
+Project 1 not neded any embedding 
+Project 2 need to using embeeding 
+This file use as placeholder
+"""
+
+
+def placeholder():
+    """Project 2 module completed।"""
+    pass
